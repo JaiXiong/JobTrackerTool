614427fba9227030eb9f8b2acf9a2800089db302
JaiLaptop <jai_1_23@hotmail.com>
2024-10-30 10:25:17 -0500

    Added employerprofile entity, made guids required

---
+++ JobEntities/Entities/EmployerProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobEntities.Entities
+{
+    public class EmployerProfile
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public JobProfile JobProfileId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+
+    }
+}
--- JobEntities/Entities/JobProfile.cs
+++ JobEntities/Entities/JobProfile.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JobEntities.Entities
 {
     public class JobProfile
     {
+        [Required]
         public Guid Id { get; set; }
         public DateOnly Date { get; set; }
 
