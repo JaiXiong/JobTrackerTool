336817ceb57b1d9313f1dd713559ce8b03f73d2c
Jai <xiongjai@gmail.com>
2024-10-30 12:50:55 -0500

    Added jobaction entity class

---
+++ JobEntities/Entities/JobAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobEntities.Entities
+{
+    public class JobAction
+    {
+        public Guid Id { get; set; }
+        public enum ActionType
+        {
+            Apply,
+            Interview,
+            Offer,
+            Reject
+        }
+        public enum Result
+        {
+            Success,
+            Failure
+        }
+    }
+}
