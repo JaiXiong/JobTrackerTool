f0a29f3bb92dc7fef967f6f3b40050400c432e1e
JaiLaptop <jai_1_23@hotmail.com>
2024-10-30 23:12:54 -0500

    Typical Angular build start

--- JobTrackerApp/src/app/app.component.ts
+++ JobTrackerApp/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatTab, MatTabGroup, MatTabsModule } from '@angular/material/tabs'; // Import MatTabsModule
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: 
+  [
+    RouterOutlet, 
+    //CommonModule, 
+    FormsModule,
+    MatTabsModule,
+    MatInputModule,
+    MatTabGroup,
+    MatTab,
+    NgIf,
+    MatIconModule,
+    //BrowserAnimationsModule,
+    //NoopAnimationsModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'JobTrackerApp';
+
+  download() {
+    console.log('Download button clicked!');
+  }
 }
