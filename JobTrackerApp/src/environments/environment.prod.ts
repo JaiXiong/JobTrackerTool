6d0a36086ff2886ec5f42f54a62cd040578a5839
JaiX <xiongjai@gmail.com>
2024-12-17 19:33:20 -0600

    fixed production to true

--- JobTrackerApp/src/environments/environment.prod.ts
+++ JobTrackerApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     jobTrackerUrl: 'http://jobtrackerapitool.azurewebsites.net', // Replace with your host API URL
     loginUrl: 'http://jobtrackerapitool.azurewebsites.net', // Replace with your host login URL
   // Add other URLs or configuration settings as needed
