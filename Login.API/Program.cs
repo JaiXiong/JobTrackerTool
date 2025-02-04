2bf277da4217b2c30749f0238c45598ae3c76b2f
JaiX <xiongjai@gmail.com>
2025-02-03 22:05:31 -0600

    trying things with swagger

--- Login.API/Program.cs
+++ Login.API/Program.cs
@@ -73,15 +73,15 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
-{
+//if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Login API v1");
         c.RoutePrefix = string.Empty;
     });
-}
+//}
 
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
