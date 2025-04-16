88ace078ad417e24f4bd8db437c606260f223d96
JaiX <xiongjai@gmail.com>
2025-04-16 11:17:41 -0500

    Created some placeholders for future test coverage

---
+++ JobTracker.API.ToolTests/Controllers/JobTrackerControllerLoginTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.API.ToolTests.Controllers
+{
+    [TestClass()]
+    public class JobTrackerControllerLoginTests
+    {
+        //TODO: Eventually mock login APIs
+    }
+}
---
+++ JobTracker.UnitTests/JobTrackerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.UnitTests
+{
+    public class JobTrackerTests
+    {
+        //TODO: Eventually create unit tests for job tracker services
+    }
+}
