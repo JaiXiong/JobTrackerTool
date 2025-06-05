178b701bca5a0fdba87d44686de4f821d9b6569e
Jai <xiongjai@gmail.com>
2025-06-05 14:41:27 -0500

    updated prod url

--- JobTrackerApp/src/environments/environment.prod.ts
+++ JobTrackerApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    jobTrackerUrl: 'https://jobtrackerapitool.azurewebsites.net', // Replace with your host API URL
+    jobTrackerUrl: 'https://jobtrackerapi-dte7aza2fvgfgaes.centralus-01.azurewebsites.net', // Replace with your host API URL
     loginUrl: 'https://jobtrackerloginapi.azurewebsites.net', // Replace with your host login URL
   // Add other URLs or configuration settings as needed
 };
